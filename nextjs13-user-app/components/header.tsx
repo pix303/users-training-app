7b60840ea14aaa13acc52a0aba3810ed65e3e718
paolo.carraro <paolo.carraro@interlogica.it>
2022-11-29 02:51:21 +0100

    Add link to home

--- nextjs13-user-app/components/header.tsx
+++ nextjs13-user-app/components/header.tsx
@@ -1,11 +1,11 @@
 'use client';
-
+import Link from "next/link";
 import Searcher from "./searcher";
 
 export default function Header() {
     return (
         <header className="bg-black p-3 px-5 flex justify-between">
-            <h1 className="text-slate-400">User next app</h1>
+            <Link href="/"><h1 className="text-slate-400">User next app</h1></Link>
             <Searcher />
         </header>
     )
