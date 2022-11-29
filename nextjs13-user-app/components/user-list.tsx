c52c6f84dc5eb1e7bcf9e023d79627aa82d65be6
paolo.carraro <paolo.carraro@interlogica.it>
2022-11-29 02:51:31 +0100

    Fix layout

--- nextjs13-user-app/components/user-list.tsx
+++ nextjs13-user-app/components/user-list.tsx
@@ -24,7 +24,7 @@ const UserList: FunctionComponent<UserListProps> = ({ users, searchName }) => {
                 <h1>User list</h1>
                 {filterdName ? <p><span>Filtered by name: </span><span className="font-bold">{filterdName}</span></p> : <p>No active filter</p>}
             </header>
-            <ul className="flex gap-10 flex-wrap">
+            <ul className="flex gap-10 flex-wrap my-6">
                 {users.map(u => {
                     return <UserPresenter key={u.id} user={u} />
                 })}
